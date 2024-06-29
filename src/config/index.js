6de5961688ca578ef1e81f9b1a0d011924e6170e
Akinrinmola Marvellous Abidemi <57664294+marvelmiles@users.noreply.github.com>
2024-06-29 21:51:03 +0100

    updated server endpoint

--- src/config/index.js
+++ src/config/index.js
@@ -3,5 +3,5 @@ console.log(process.env.NODE_ENV, "....netlify site mode");
 export const isProdMode = process.env.NODE_ENV === "production";
 
 export const API_ENDPOINT = isProdMode
-  ? "https://glory-shelled-crow.glitch.me/api" //"https://enormously-sure-werewolf.ngrok-free.app/api" //"https://caltex-api.onrender.com/api"
+  ? "https://caltex-api.glitch.me/api" //"https://enormously-sure-werewolf.ngrok-free.app/api" //"https://caltex-api.onrender.com/api"
   : "http://localhost:8080/api";

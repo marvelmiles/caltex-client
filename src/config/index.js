d2420ec593382ce39281878be3652c2d76365177
marvelmiles <marvellousabidemi2@gmail.com>
2025-07-21 21:34:52 +0100

    preparing to switch to render

--- src/config/index.js
+++ src/config/index.js
@@ -1,5 +1,5 @@
 export const isProdMode = process.env.NODE_ENV === "production";
 
 export const API_ENDPOINT = isProdMode
-  ? "https://caltex-api-yzhj.onrender.com" //"https://enormously-sure-werewolf.ngrok-free.app/api"
-  : "https://caltex-api-yzhj.onrender.com"; //"http://localhost:8080/api";
+  ? "https://caltex-api-yzhj.onrender.com/api" //"https://enormously-sure-werewolf.ngrok-free.app/api"
+  : "https://caltex-api-yzhj.onrender.com/api"; //"http://localhost:8080/api";
